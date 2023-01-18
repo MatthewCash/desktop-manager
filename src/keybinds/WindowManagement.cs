238529b02499103f2bf04f78e73de728e8c7c9e3
Matthew_Cash <matthew@matthew-cash.com>
2023-01-17 18:07:43 -0800

    Add window management keybinds

---
+++ src/keybinds/WindowManagement.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using Gma.System.MouseKeyHook;
+
+static class WindowManagement {
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
+    const int SW_MAXIMIZE = 3;
+    const int SW_MINIMIZE = 6;
+    const int SW_NORMAL = 1;
+
+    public static void MouseDown(object sender, MouseEventExtArgs e) {
+        if (Keybinds.trackedKeyStates[Keys.LControlKey] || Keybinds.trackedButtonStates[MouseButtons.Middle]) {
+            if (e.Button == MouseButtons.XButton1) {
+                e.Handled = true;
+
+                void handler(object sender, MouseEventExtArgs e) {
+                    Keybinds.trackedButtonStates[e.Button] = false;
+                    if (e.Button != MouseButtons.XButton1) return;
+                    e.Handled = true;
+                    Keybinds.inputHook.MouseUpExt -= handler;
+                };
+                Keybinds.inputHook.MouseUpExt += handler;
+
+                if (Keybinds.trackedKeyStates[Keys.LMenu]) {
+                    // Restore
+                    ShowWindowAsync(GetForegroundWindow(), SW_NORMAL);
+                } else {
+                    // Minimize
+                    ShowWindowAsync(GetForegroundWindow(), SW_MINIMIZE);
+                }
+            }
+            if (e.Button == MouseButtons.XButton2) {
+                e.Handled = true;
+
+                void handler(object sender, MouseEventExtArgs e) {
+                    Keybinds.trackedButtonStates[e.Button] = false;
+                    if (e.Button != MouseButtons.XButton2) return;
+                    e.Handled = true;
+                    Keybinds.inputHook.MouseUpExt -= handler;
+                };
+                Keybinds.inputHook.MouseUpExt += handler;
+
+                ShowWindowAsync(GetForegroundWindow(), SW_MAXIMIZE);
+            }
+        }
+    }
+}
