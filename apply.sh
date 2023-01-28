301acd7a0cdfd4d7d07a76fb5a6f07704fe9be8e
Matthew_Cash <matthew@matthew-cash.com>
2023-01-27 17:57:43 -0800

    Move binary patch to build script

--- apply.sh
+++ apply.sh
@@ -5,9 +5,6 @@
 ssh host "schtasks /End /TN DesktopManager"
 sleep 1
 
-# Patch the binary to make it WinExe instead of Console
-printf '\x67\x64\x02\x00\x02' | dd of=./bin/win-x64/publish/DesktopManager.exe bs=1 seek=320 count=5 conv=notrunc
-
 # Copy changed files
 rsync -a ./bin/win-x64/publish/* /mnt/win/Users/Matthew/bin/DesktopManager/
 
--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 #! nix-shell -i bash -p dotnet-sdk
 
 dotnet publish -c Release
+
+# Patch the binary to make it WinExe instead of Console
+printf '\x67\x64\x02\x00\x02' | dd of=./bin/win-x64/publish/DesktopManager.exe bs=1 seek=320 count=5 conv=notrunc
