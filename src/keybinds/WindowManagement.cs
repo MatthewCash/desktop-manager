b8067e3a00b3043146cd684826a7d62a8d96a24e
Matthew_Cash <matthew@matthew-cash.com>
2023-12-13 15:39:39 -0800

    Change maximize action to restore windows that are already maximized

--- src/keybinds/WindowManagement.cs
+++ src/keybinds/WindowManagement.cs
@@ -10,6 +10,20 @@ class WindowManagement : Keybinds.IKeybindHandler {
     [DllImport("user32.dll")]
     static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPLACEMENT {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public System.Drawing.Point ptMinPosition;
+        public System.Drawing.Point ptMaxPosition;
+        public System.Drawing.Rectangle rcNormalPosition;
+    }
+
+
+    [DllImport("user32.dll")]
+    public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
     const int SW_MAXIMIZE = 3;
     const int SW_MINIMIZE = 6;
     const int SW_NORMAL = 1;
@@ -22,47 +36,59 @@ class WindowManagement : Keybinds.IKeybindHandler {
         return Config.GetConfig().DisabledWindows.Contains(className);
     }
 
+    private static bool IsWindowMaximized(IntPtr hWnd) {
+        WINDOWPLACEMENT placement = new() { length = Marshal.SizeOf<WINDOWPLACEMENT>() };
+        GetWindowPlacement(hWnd, ref placement);
+        return placement.showCmd == SW_MAXIMIZE;
+    }
+
     public void MouseDown(object sender, MouseEventExtArgs e) {
         if (
-            Keybinds.trackedKeyStates[Keys.LControlKey] ||
-            Keybinds.trackedButtonStates[MouseButtons.Middle]
-        ) {
-            if (e.Button == MouseButtons.XButton1) {
+            !(Keybinds.trackedKeyStates[Keys.LControlKey] ||
+            Keybinds.trackedButtonStates[MouseButtons.Middle])
+        ) return;
+
+        if (e.Button == MouseButtons.XButton1) {
+            e.Handled = true;
+
+            void handler(object sender, MouseEventExtArgs e) {
+                Keybinds.trackedButtonStates[e.Button] = false;
+                if (e.Button != MouseButtons.XButton1) return;
                 e.Handled = true;
+                Keybinds.inputHook.MouseUpExt -= handler;
+            };
+            Keybinds.inputHook.MouseUpExt += handler;
 
-                void handler(object sender, MouseEventExtArgs e) {
-                    Keybinds.trackedButtonStates[e.Button] = false;
-                    if (e.Button != MouseButtons.XButton1) return;
-                    e.Handled = true;
-                    Keybinds.inputHook.MouseUpExt -= handler;
-                };
-                Keybinds.inputHook.MouseUpExt += handler;
+            var hWnd = GetForegroundWindow();
+            if (ShouldIgnoreWindow(hWnd)) return;
 
-                var hWnd = GetForegroundWindow();
-                if (ShouldIgnoreWindow(hWnd)) return;
-
-                if (Keybinds.trackedKeyStates[Keys.LMenu]) {
-                    // Restore
-                    ShowWindowAsync(hWnd, SW_NORMAL);
-                } else {
-                    // Minimize
-                    ShowWindowAsync(hWnd, SW_MINIMIZE);
-                }
+            if (Keybinds.trackedKeyStates[Keys.LMenu]) {
+                // Restore
+                ShowWindowAsync(hWnd, SW_NORMAL);
+            } else {
+                // Minimize
+                ShowWindowAsync(hWnd, SW_MINIMIZE);
             }
-            if (e.Button == MouseButtons.XButton2) {
+        }
+        if (e.Button == MouseButtons.XButton2) {
+            e.Handled = true;
+
+            void handler(object sender, MouseEventExtArgs e) {
+                Keybinds.trackedButtonStates[e.Button] = false;
+                if (e.Button != MouseButtons.XButton2) return;
                 e.Handled = true;
+                Keybinds.inputHook.MouseUpExt -= handler;
+            };
+            Keybinds.inputHook.MouseUpExt += handler;
 
-                void handler(object sender, MouseEventExtArgs e) {
-                    Keybinds.trackedButtonStates[e.Button] = false;
-                    if (e.Button != MouseButtons.XButton2) return;
-                    e.Handled = true;
-                    Keybinds.inputHook.MouseUpExt -= handler;
-                };
-                Keybinds.inputHook.MouseUpExt += handler;
+            var hWnd = GetForegroundWindow();
+            if (ShouldIgnoreWindow(hWnd)) return;
 
-                var hWnd = GetForegroundWindow();
-                if (ShouldIgnoreWindow(hWnd)) return;
-
+            if (IsWindowMaximized(hWnd)) {
+                // Restore
+                ShowWindowAsync(hWnd, SW_NORMAL);
+            } else {
+                // Maximize
                 ShowWindowAsync(hWnd, SW_MAXIMIZE);
             }
         }
