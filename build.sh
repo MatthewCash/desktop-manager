8cd5ee54c6a76f683c786a296739b264511caea2
Matthew_Cash <matthew@matthew-cash.com>
2024-06-18 23:02:21 -0700

    Fix binary patch
    Hopefully for the last time :)

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p bash dotnet-sdk
+#! nix-shell -i bash -p bash dotnet-sdk gnugrep coreutils
 
 dotnet publish -c Release
 
+bin_path="./bin/win-x64/publish/DesktopManager.exe"
+
 # Patch the binary to make it WinExe instead of Console
-printf '\x02\x00' | dd of=./bin/win-x64/publish/DesktopManager.exe bs=1 seek=332 count=2 conv=notrunc
+offset=$(grep -obaP -m 1 'PE\x00\x00' "$bin_path" | cut -d: -f1)
+printf '\x02\x00' | dd of="$bin_path" bs=1 seek=$((offset + 0x5c)) count=2 conv=notrunc
